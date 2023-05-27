47a11fe8f1c15eaebb55b82a3ad98556956dd9cb
Ken Diani <kendiani@students.amikom.ac.id>
2023-05-28 01:18:13 +0700

    change expired token time

--- controllers/Users.js
+++ controllers/Users.js
@@ -2,10 +2,10 @@ import Users from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const getUsers = async(req, res) => {
+export const getUsers = async (req, res) => {
     try {
         const users = await Users.findAll({
-            attributes:['id','name','email']
+            attributes: ['id', 'name', 'email']
         });
         res.json(users);
     } catch (error) {
@@ -13,9 +13,9 @@ export const getUsers = async(req, res) => {
     }
 }
 
-export const Register = async(req, res) => {
+export const Register = async (req, res) => {
     const { name, email, password, confPassword } = req.body;
-    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
+    if (password !== confPassword) return res.status(400).json({ msg: "Password dan Confirm Password tidak cocok" });
     const salt = await bcrypt.genSalt();
     const hashPassword = await bcrypt.hash(password, salt);
     try {
@@ -24,42 +24,42 @@ export const Register = async(req, res) => {
             email: email,
             password: hashPassword
         });
-        res.json({msg: "Register Berhasil"});
+        res.json({ msg: "Register Berhasil" });
     } catch (error) {
         if (error.name === "SequelizeUniqueConstraintError") {
-            return res.status(400).json({msg: "Alamat email sudah ada yang menggunakan"})
+            return res.status(400).json({ msg: "Alamat email sudah ada yang menggunakan" })
         } else {
-            return res.status(400).json({msg: "Error"})
+            return res.status(400).json({ msg: "Error" })
         }
     }
 }
 
-export const Login = async(req, res) => {
+export const Login = async (req, res) => {
     try {
         const user = await Users.findAll({
-            where:{
+            where: {
                 email: req.body.email
             }
         });
         const match = await bcrypt.compare(req.body.password, user[0].password);
-        if(!match) return res.status(400).json({msg: "Wrong Password"});
+        if (!match) return res.status(400).json({ msg: "Wrong Password" });
         const userId = user[0].id;
         const name = user[0].name;
         const email = user[0].email;
-        const accessToken = jwt.sign({userId, name, email}, process.env.ACCESS_TOKEN_SECRET,{
-            expiresIn: '20s'
-        });
-        const refreshToken = jwt.sign({userId, name, email}, process.env.REFRESH_TOKEN_SECRET,{
+        const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN_SECRET, {
             expiresIn: '1d'
         });
-        await Users.update({refresh_token: refreshToken},{
-            where:{
+        const refreshToken = jwt.sign({ userId, name, email }, process.env.REFRESH_TOKEN_SECRET, {
+            expiresIn: '1d'
+        });
+        await Users.update({ refresh_token: refreshToken }, {
+            where: {
                 id: userId
             }
         });
 
         res.json({ accessToken, refreshToken });
     } catch (error) {
-        res.status(404).json({msg:"Email tidak ditemukan"});
+        res.status(404).json({ msg: "Email tidak ditemukan" });
     }
 }
